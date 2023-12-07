cf34c9b362c2a8f012622f7ba325c23cbfe9932c
Monviech <79600909+Monviech@users.noreply.github.com>
2023-12-07 16:02:58 +0100

    Update ReverseProxyController.php
    Refactored for UI-Bootgrid Plugin

--- usr/plugins/devel/caddy/src/opnsense/mvc/app/controllers/Pischem/Caddy/Api/ReverseProxyController.php
+++ usr/plugins/devel/caddy/src/opnsense/mvc/app/controllers/Pischem/Caddy/Api/ReverseProxyController.php
@@ -3,7 +3,7 @@
 /**
  *    Copyright (C) 2015 Deciso B.V.
  *    Copyright (C) 2023 Cedrik Pischem
- * 
+ *
  *    All rights reserved.
  *
  *    Redistribution and use in source and binary forms, with or without
@@ -33,151 +33,39 @@
 namespace Pischem\Caddy\Api;
 
 use OPNsense\Base\ApiMutableModelControllerBase;
-use OPNsense\Core\Config;
 
 class ReverseProxyController extends ApiMutableModelControllerBase
 {
     protected static $internalModelName = 'caddy';
     protected static $internalModelClass = 'Pischem\Caddy\Caddy';
 
-    // Since we're only dealing with the reverse proxy section, we don't need a filter or wrapDefaults.
-
-    public function searchAction()
+    public function searchReverseProxyAction()
     {
-        // Automatically return all reverse proxy entries without specifying fields
-        return $this->searchBase('reverseproxy.reverse', array_keys(get_object_vars($this->getModel()->reverseproxy->reverse)));
+        return $this->searchBase("reverseproxy.reverse", ['enabled', 'FromDomain', 'FromPort', 'ToDomain', 'ToPort', 'Description', 'DnsChallenge']);
     }
 
-    public function getAction($uuid = null)
+    public function setReverseProxyAction($uuid)
     {
-        $mdlCaddy = new \Pischem\Caddy\Caddy();
-        if ($uuid !== null) {
-            // Fetch that specific reverse proxy setting
-            $node = $mdlCaddy->getNodeByReference('reverseproxy.reverse.' . $uuid);
-            if ($node != null) {
-                return ['reverse' => [$uuid => $node->getNodes()]];
-            }
-            return []; // Return an empty array if the UUID is not found
-        } else {
-            // Fetch all reverse proxy settings indexed by UUID
-            $result = [];
-            foreach ($mdlCaddy->reverseproxy->reverse->iterateItems() as $node) {
-                $uuid = $node->getAttributes()['uuid'];
-                $result[$uuid] = $node->getNodes();
-            }
-            return ['reverse' => $result];
-        }
-    }
-    
-    
-    public function setAction($uuid = null)
-    {
-        if ($this->request->isPut()) {
-            $rawPutData = $this->request->getRawBody();
-            $putData = json_decode($rawPutData, true);
-            if ($putData === null || !isset($putData['reverse'])) {
-                return ["result" => "failed", "validations" => ["Invalid input format"]];
-            }
-            
-            // If the UUID is not provided in the URL, attempt to extract it from the data
-            if ($uuid === null && isset($putData['reverse']['uuid'])) {
-                $uuid = $putData['reverse']['uuid'];
-            }
-            
-            if ($uuid === null) {
-                return ["result" => "failed", "validations" => ["UUID not provided"]];
-            }
-            
-            // Fetch the node reference to update
-            $node = $this->getModel()->getNodeByReference('reverseproxy.reverse.' . $uuid);
-            if ($node != null) {
-                // Overwrite the node's data with the new data
-                foreach ($putData['reverse'] as $key => $value) {
-                    $node->$key = $value;
-                }
-                
-                // Save the changes if the model is valid
-                $validationMessages = $this->getModel()->performValidation();
-                $errors = [];
-                if ($validationMessages->count() > 0) {
-                    // Collect validation errors
-                    foreach ($validationMessages as $message) {
-                        // Extract the last part of the field name
-                        $fieldParts = explode('.', $message->getField());
-                        $fieldName = end($fieldParts);
-                        $errors["reverse.$fieldName"] = $message->getMessage();
-                    }
-                    return ["result" => "failed", "validations" => $errors];
-                } else {
-                    // No validation errors, save model
-                    $this->getModel()->serializeToConfig();
-                    Config::getInstance()->save();
-                    return ["result" => "saved", "message" => "Reverse proxy entry updated"];
-                }
-            } else {
-                return ["result" => "failed", "validations" => ["Reverse proxy entry with the specified UUID not found"]];
-            }
-        } else {
-            return ["result" => "failed", "validations" => ["Only PUT method is allowed for this endpoint"]];
-        }
+        return $this->setBase("reverse", "reverseproxy.reverse", $uuid);
     }
 
-
-    public function addAction()
+    public function addReverseProxyAction()
     {
-        // This function will add a new reverse proxy entry and automatically generate a UUID
-        $result = $this->addBase('reverse', 'reverseproxy.reverse');
-        Config::getInstance()->save(); // Save the configuration to make sure the UUID is persisted
-        return $result;
+        return $this->addBase("reverse", "reverseproxy.reverse");
     }
 
-    public function toggleAction($uuid, $enabled = null)
+    public function getReverseProxyAction($uuid = null)
     {
-        // This function will enable or disable a specific reverse proxy entry
-        return $this->toggleBase('reverseproxy.reverse', $uuid, $enabled);
+        return $this->getBase("reverse", "reverseproxy.reverse", $uuid);
     }
 
-    public function delAction($uuid)
+    public function delReverseProxyAction($uuid)
     {
-    if ($this->request->isPost()) {
-        if ($uuid == null) {
-            $uuid = $this->request->getPost("uuid");
-        }
+        return $this->delBase("reverseproxy.reverse", $uuid);
+    }
 
-        // Check if UUID is provided
-        if ($uuid != null) {
-            // Fetch node reference
-            $node = $this->getModel()->getNodeByReference('reverseproxy.reverse.' . $uuid);
-
-            if ($node != null) {
-                // Use the internal delete method
-                $this->getModel()->reverseproxy->reverse->del($uuid);
-                if ($this->getModel()->performValidation()->count() == 0) {
-                    // Save changes if validation passed
-                    $this->getModel()->serializeToConfig();
-                    Config::getInstance()->save();
-
-                    // Respond with success
-                    return ["status" => "ok"];
-                } else {
-                    // Respond with validation errors
-                    $validationMessages = $this->getModel()->performValidation();
-                    foreach ($validationMessages as $message) {
-                        return ["status" => "failed", "message" => $message];
-                    }
-                }
-            } else {
-                // Respond with error if node doesn't exist
-                return ["status" => "failed", "message" => "Reverse proxy entry not found."];
-            }
-        } else {
-            // Respond with error if UUID isn't provided
-            return ["status" => "failed", "message" => "No UUID provided for deletion."];
-        }
-    } else {
-        // Respond with error if not a POST request
-        return ["status" => "failed", "message" => "Invalid request method."];
-        }
+    public function toggleReverseProxyAction($uuid, $enabled = null)
+    {
+        return $this->toggleBase("reverseproxy.reverse", $uuid, $enabled);
     }
 }
-
