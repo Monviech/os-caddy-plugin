86f39c642719358907ddfc858dea6aea379f1997
Monviech <79600909+Monviech@users.noreply.github.com>
2023-12-09 12:55:21 +0100

    Update ReverseProxyController.php
    Implemented Subdomain API calls

--- usr/plugins/devel/caddy/src/opnsense/mvc/app/controllers/Pischem/Caddy/Api/ReverseProxyController.php
+++ usr/plugins/devel/caddy/src/opnsense/mvc/app/controllers/Pischem/Caddy/Api/ReverseProxyController.php
@@ -44,7 +44,7 @@ class ReverseProxyController extends ApiMutableModelControllerBase
 
     public function searchReverseProxyAction()
     {
-        return $this->searchBase("reverseproxy.reverse", ['enabled', 'FromDomain', 'FromPort', 'ToDomain', 'ToPort', 'Description', 'DnsChallenge']);
+        return $this->searchBase("reverseproxy.reverse", ['enabled', 'FromDomain', 'FromPort', 'Description', 'DnsChallenge']);
     }
 
     public function setReverseProxyAction($uuid)
@@ -73,11 +73,44 @@ class ReverseProxyController extends ApiMutableModelControllerBase
     }
 
 
+    /*Subdomain Section*/
+
+        public function searchSubdomainAction()
+    {
+        return $this->searchBase("reverseproxy.subdomain", ['enabled', 'reverse', 'FromDomain', 'FromPort', 'Description']);
+    }
+
+    public function setSubdomainAction($uuid)
+    {
+        return $this->setBase("subdomain", "reverseproxy.subdomain", $uuid);
+    }
+
+    public function addSubdomainAction()
+    {
+        return $this->addBase("subdomain", "reverseproxy.subdomain");
+    }
+
+    public function getSubdomainAction($uuid = null)
+    {
+        return $this->getBase("subdomain", "reverseproxy.subdomain", $uuid);
+    }
+
+    public function delSubdomainAction($uuid)
+    {
+        return $this->delBase("reverseproxy.subdomain", $uuid);
+    }
+
+    public function toggleSubdomainAction($uuid, $enabled = null)
+    {
+        return $this->toggleBase("reverseproxy.subdomain", $uuid, $enabled);
+    }
+
+
     /*Handle Section*/
 
         public function searchHandleAction()
     {
-        return $this->searchBase("reverseproxy.handle", ['enabled', 'reverse', 'HandleType', 'HandlePath', 'ToDomain', 'ToPort', 'Description']);
+        return $this->searchBase("reverseproxy.handle", ['enabled', 'reverse', 'subdomain', 'HandleType', 'HandlePath', 'ToDomain', 'ToPort', 'Description']);
     }
 
     public function setHandleAction($uuid)
