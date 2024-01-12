655d0e17e656f95cc38c6d1af3633f79382646c0
Monviech <gitacc@pischem.com>
2024-01-12 13:58:49 +0000

    Build os-caddy-1.4.0. Logging refactored to syslog-ng, HTTP Access Logs can be enabled, Dynamic DNS feature added, new DNS Provider added.

--- usr/local/share/licenses/caddy-2.7.6/catalog.mk
+++
@@ -1,6 +0,0 @@
-_LICENSE=APACHE20
-_LICENSE_NAME=Apache License 2.0
-_LICENSE_PERMS=dist-mirror dist-sell pkg-mirror pkg-sell auto-accept
-_LICENSE_GROUPS=APACHE OSI
-_LICENSE_DISTFILES=caddy-2.7.6.pkg
-
